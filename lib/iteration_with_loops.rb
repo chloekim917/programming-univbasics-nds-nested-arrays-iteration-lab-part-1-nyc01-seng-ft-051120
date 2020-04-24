535510f0667726ccb91f98fdbb5eedae2d55b530
Seoyoung Kim <mintholic1@gmail.com>
2020-04-24 19:33:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,12 @@ def find_even_values(src)
   # Output all even values in each nested array
   row_index = 0 
   while row_index < src.count do
-    while 
+    inner_index = 0
+    while inner_index < src[row_index].count do
+      if src[row_index][inner_index] % 2 == 50
+        p src[row_index][inner_index]
+        inner_index += 1
+      end
+      row_index += 1
 
 end
